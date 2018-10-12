b027ec990af1dc5bf7239c2ac95c02d7be6e6421
Justin van de laar <justin555@live.nl>
2018-10-12 11:28:31 +0200

    First push

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include ("templates/header.php"); ?>
+
+<div>
+    <p>test</p>
+    <p> Lorem ipesum</p>
+</div>
+
+<?php include ("templates/footer.php"); ?>
---
+++ templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>OIM 11 portfolio </title>
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src=""></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<ul class="menu">
+    <li class="menu1Li"><a class="menu1A" href="index.php"> Home </a></li>
+    <li class="menu1Li"><a class="menu1A" href="#">About me <i class="fa fa-caret-down"></i></a>
+    <ul class="sub-menu">
+        <li class="subLi"><a class="subA" href="">  MBO history</a> </li>
+        <li class="subLi"><a class="subA" href="">  Hobbys </a> </li>
+        <li class="subLi"><a class="subA" href="">  CV</a> </li>
+        <li class="subLi"><a class="subA" href="">  Passions </a> </li>
+    </ul></li>
+    <li class="menu1Li"><a class="menu1A" href="#">OIM opdrachten <i class="fa fa-caret-down"></i></a>
+        <ul class="sub-menu">
+            <li class="subLi"><a class="subA" href="">  A day in the life</a></li>
+            <li class="subLi"><a class="subA" href="">  No logo</a></li>
+            <li class="subLi"><a class="subA" href="">  Big data</a></li>
+            <li class="subLi"><a class="subA" href="">  We are the robots </a></li>
+            <li class="subLi"><a class="subA" href="">  User experience </a></li>
+        </ul>
+    </li>
+    <li class="menu1Li"><a class="menu1A" href="#">Social network</a></li>
+</ul>
+<body>
