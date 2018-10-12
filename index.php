1fb52675eecce6da4a29872e82ea84875bcf080c
Justin van de laar <justin555@live.nl>
2018-10-12 12:33:57 +0200

    Set the homestyle for the website

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php include ("templates/header.php"); ?>
 
 <div>
-    <p>test</p>
-    <p> Lorem ipesum</p>
+    <h1> Welcome to my portfolio </h1>
+    <p> u are welcome on my portfolio <br>
+    use the menu to navigate through the website</p>
 </div>
 
 <?php include ("templates/footer.php"); ?>
